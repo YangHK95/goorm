b370b7ae226c8c5931678fa455f2cbd99a2b3c59
heekwang <ekzm7040@naver.com>
2023-11-21 14:33:53 +0900

    얕은 복사 & 깊은 복사

--- javascript/closure/script.js
+++ javascript/closure/script.js
@@ -1,18 +1,59 @@
-// function outerFunction(outerVariable) {
-//     return function innerFunction(innerVariable) {
-//         console.log('Outer function: ' + outerVariable)
-//         console.log('Inner function:' + innerVariable)
-//     }
+// // shallow copy
+// const aArray = [1, 2, 3]
+
+// // shallow copy with spread operator
+// const bArray = [...aArray, 4]
+// console.log('aAray', aArray)
+// console.log('bAray', bArray)
+// console.log('aArray === bArray', aArray === bArray)
+
+// // shallow copy with Object.assign()
+// const cArray = Object.assign([], bArray)
+// console.log('cArray', cArray)
+// console.log('aArray === cArray', aArray === cArray)
+
+// // Nested Araay and Object
+// cArray.push([5, 6, 7])
+// console.log('cArray', cArray)
+// const dArray = [...cArray, 10]
+// console.log('dArray', dArray)
+// dArray[4].push(8)
+// console.log('cArray', cArray)
+// console.log('dArray', dArray)
+
+// //얕은 동결
+// const aObject = {
+//     "a": "a",
+//     "b": "b",
+//     "cObject": { "a": 1, "b": 2 }
 // }
 
-// const newFunction = outerFunction('outerVariable')
-// newFunction('innerVariable')
+// Object.freeze(aObject)
+// aObject.a = "c"
+// console.log('aObject', aObject)
 
-let a = 'a'
+// aObject.cObject.a = 3;
+// console.log('aObject', aObject)
 
-function functionA() {
-    let b = 'b'
-    console.log(a, b)
+const aObject = {
+    "a": "a",
+    "b": "b",
+    "cObject": { "a": 1, "b": 2 }
 }
 
-functionA()
+// deep copy with Json.parse(json.stringify())
+// const newAOject = JSON.parse(JSON.stringify(aObject))
+
+// console.log('aObject', aObject)
+// console.log('newAOject', newAOject)
+// aObject.cObject.a = 3;
+// console.log('aObject', aObject)
+// console.log('newAOject', newAOject)
+
+//deep copy with nested spread operator
+const newAOject = { ...aObject, cObject: { ...aObject.cObject } }
+console.log('aObject', aObject)
+console.log('newAOject', newAOject)
+aObject.cObject.a = 3;
+console.log('aObject', aObject)
+console.log('newAOject', newAOject)
