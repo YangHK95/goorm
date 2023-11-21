1171b7138f27dbb6c0b3aa20d7c52ffffffc10f3
heekwang <ekzm7040@naver.com>
2023-11-21 12:15:23 +0900

    closure

---
+++ javascript/closure/script.js
@@ -0,0 +1,18 @@
+// function outerFunction(outerVariable) {
+//     return function innerFunction(innerVariable) {
+//         console.log('Outer function: ' + outerVariable)
+//         console.log('Inner function:' + innerVariable)
+//     }
+// }
+
+// const newFunction = outerFunction('outerVariable')
+// newFunction('innerVariable')
+
+let a = 'a'
+
+function functionA() {
+    let b = 'b'
+    console.log(a, b)
+}
+
+functionA()
