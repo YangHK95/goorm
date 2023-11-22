d537ee9437c4ab723dceff29fa2a325a8d5573af
heekwang <ekzm7040@naver.com>
2023-11-22 17:51:11 +0900

    generator

--- javascript/iterator,generator/script.js
+++ javascript/iterator,generator/script.js
@@ -32,10 +32,59 @@
 //     console.log(n)
 // }
 
-const set = new Set([1, 2, 3, 4, 5])
-console.log('set', set)
-const map = new Map([['a', 1], ['b', 2]])
-console.log('map', map)
+// const set = new Set([1, 2, 3, 4, 5])
+// console.log('set', set)
+// const map = new Map([['a', 1], ['b', 2]])
+// console.log('map', map)
 
-console.log(set[Symbol.iterator]().next())
-console.log(map[Symbol.iterator]().next())
+// console.log(set[Symbol.iterator]().next())
+
+// // generate
+// // generate 함수
+// function* sayNumbers() {
+//     yield 1;
+//     yield 2;
+//     yield 3;
+// }
+// // 제너레이터 함수의 반환이 젠너레이터
+// const number = sayNumbers()
+
+// console.log(number.next())
+// console.log(number.next())
+// console.log(number.next())
+// console.log(number.next())
+// console.log(number.next())
+
+// function* generatorFunction() {
+//     yield 1;
+// }
+// const generator = generatorFunction()
+
+// // generator = generator[Symbol.iterator]
+
+// console.log(generator.next())
+
+// // ------사용 방법-----
+// function* createIds() {
+//     let index = 1;
+
+//     while (true) {
+//         yield index++;
+//     }
+// }
+
+// const gen = createIds()
+// console.log(gen.next().value)
+// console.log(gen.next().value)
+// console.log(gen.next().value)
+// console.log(gen.return(10))
+
+function* generatorFunction() {
+    yield* [1, 2, 3]
+}
+
+const generator = generatorFunction()
+
+for (const number of generator) {
+    console.log(number)
+}
