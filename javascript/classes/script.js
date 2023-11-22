72cadc4639ac450b6e53e62178f2ff30988880c1
heekwang <ekzm7040@naver.com>
2023-11-22 12:36:43 +0900

    sub&super

--- javascript/classes/script.js
+++ javascript/classes/script.js
@@ -1,17 +1,38 @@
-class Person {
-    constructor(name, email, birthday) {
-        this.mane = name;
-        this.email = email;
-        this.birthday = new Date(birthday);
-    }
+// class Person {
+//     constructor(name, email, birthday) {
+//         this.mane = name;
+//         this.email = email;
+//         this.birthday = new Date(birthday);
+//     }
 
-    introdouce() {
-        return `Heelo my name is ${this.name}`;
+//     introdouce() {
+//         return `Heelo my name is ${this.name}`;
+//     }
+//     static multipleNumbers(x, y) {
+//         return x * y;
+//     }
+// }
+
+// const john = new Person('john', 'jonh@example.com', '10-3-98')
+// console.log(john)
+
+class Person {
+    constructor(name, email) {
+        this.name = name;
+        this.email = email;
     }
-    static multipleNumbers(x, y) {
-        return x * y;
+    introduce() {
+        return `Hello my name is ${this.name}`;
     }
 }
 
-const john = new Person('john', 'jonh@example.com', '10-3-98')
-console.log(john)
+class Client extends Person {
+    constructor(name, email, phone, address) {
+        super(name, email)
+
+        this.phone = phone;
+        this.address = address;
+    }
+}
+const john = new Client('john', 'jonh@example.com', '010-0000-0000', '서울');
+console.log(john.introduce());
