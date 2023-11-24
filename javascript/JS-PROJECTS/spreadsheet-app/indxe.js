2c9b013e556f006868c112175bb286aeeba2e883
heekwang <ekzm7040@naver.com>
2023-11-24 17:56:57 +0900

    셀 상태 나타내기

--- javascript/JS-PROJECTS/spreadsheet-app/indxe.js
+++ javascript/JS-PROJECTS/spreadsheet-app/indxe.js
@@ -118,6 +118,7 @@ function handleCellClick(cell) {
     const rowHeaderEl = getElFromRowCol(rowHeader.row, rowHeader.column)
     columnHeaderEl.classList.add('active')
     rowHeaderEl.classList.add('active')
+    document.querySelector("#cell-status").innerHTML = cell.columnName + cell.rowName
 
     // console.log('clicked cell', columnHeaderEl, rowHeaderEl)
 
