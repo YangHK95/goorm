7f3afeb0958d0841950893d5f4f60286723b124b
heekwang <ekzm7040@naver.com>
2023-11-24 11:21:12 +0900

    기본 데티어 생성하기

--- javascript/JS-PROJECTS/spreadsheet-app/indxe.js
+++ javascript/JS-PROJECTS/spreadsheet-app/indxe.js
@@ -1,4 +1,17 @@
-const spreadSheetContainer = dpcument.querySelector('#spreadsheet-container');
+const spreadSheetContainer = document.querySelector('#spreadsheet-container');
 const ROWS = 10
 const COLS = 10
 const spreadsheet = []
+
+initSpreadsheet()
+
+function initSpreadsheet() {
+    for (let i = 0; i < ROWS; i++) {
+        let spreadsheetRow = []
+        for (let j = 0; j < COLS; j++) {
+            spreadsheetRow.push(i + "-" + j)
+        }
+        spreadsheet.push(spreadsheetRow)
+    }
+    console.log(spreadsheet)
+}
