82a49a724454375397bd1e95cd2d342549a1768c
heekwang <ekzm7040@naver.com>
2023-11-30 19:02:44 +0900

    github finder

--- javascript/JS-PROJECTS/Github Finder copy 2/github.js
+++ javascript/JS-PROJECTS/Github Finder copy 2/github.js
@@ -12,8 +12,10 @@ class Github {
                 `https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`
             );
 
-        const repoResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`);
-
+        const repoResponse =
+            await fetch(
+                `https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`
+            );
 
         const profile = await profileResponse.json();
         const repos = await repoResponse.json();
@@ -22,8 +24,5 @@ class Github {
             profile,
             repos
         }
-
     }
-
 }
-
