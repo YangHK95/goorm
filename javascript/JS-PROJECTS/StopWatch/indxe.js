80aa6bccf3d6511ff1ccb600cd1d632cc24a324d
heekwang <ekzm7040@naver.com>
2023-12-01 10:35:16 +0900

    practice stopwatch

--- javascript/JS-PROJECTS/StopWatch/indxe.js
+++ javascript/JS-PROJECTS/StopWatch/indxe.js
@@ -1,43 +1,86 @@
-const appendTens = document.getElementById('tens');
+// const appendTens = document.getElementById('tens');
+// const appendSeconds = document.getElementById('seconds');
+// const buttonStart = document.getElementById('button-start');
+// const buttonStop = document.getElementById('button-stop');
+// const buttonReset = document.getElementById('button-reset');
+
+// let seconds = 0
+// let tens = 0
+// let interval
+// // 인터벌 아이디를 만들어야 
+
+// buttonStart.onclick = function () {
+//     clearInterval(interval)
+//     interval = setInterval(startTimer, 10)
+
+// }
+
+// buttonStop.onclick = function () {
+//     clearInterval(interval)
+// }
+// buttonReset.onclick = function () {
+//     clearInterval(interval)
+
+//     seconds = 0
+//     tens = 0
+//     appendSeconds.innerText = seconds
+//     appendTens.innerHTML = tens
+// }
+// function startTimer() {
+//     tens++
+
+//     if (tens > 99) {
+//         // seonds 1 올리기
+//         seconds++
+//         // appendSeconds 에도 반영하기
+//         appendSeconds.innerText = seconds
+//         // tens, appendTens => 0
+//         tens = 0
+//         appendTens.innerText = tens
+//     } else {
+//         appendTens.innerText = tens
+//     }
+// }
+
 const appendSeconds = document.getElementById('seconds');
-const buttonStart = document.getElementById('button-start');
-const buttonStop = document.getElementById('button-stop');
-const buttonReset = document.getElementById('button-reset');
+const appendTens = document.getElementById('tens');
+const buttonStart = document.getElementById('button-start')
+const buttonStop = document.getElementById('button-stop')
+const buttonReset = document.getElementById('button-reset')
 
 let seconds = 0
 let tens = 0
 let interval
-// 인터벌 아이디를 만들어야 
+
 
 buttonStart.onclick = function () {
     clearInterval(interval)
-    interval = setInterval(startTimer, 10)
-
+    interval = setInterval(startTimer, 10);
 }
 
 buttonStop.onclick = function () {
     clearInterval(interval)
 }
-buttonReset.onclick = function () {
-    clearInterval(interval)
 
-    seconds = 0
-    tens = 0
-    appendSeconds.innerText = seconds
-    appendTens.innerHTML = tens
+buttonReset.onclick = function () {
+    clearInterval(interval);
+
+    seconds = 0;
+    tens = 0;
+    appendSeconds.innerText = seconds;
+    appendTens.innerText = tens
 }
+
+
 function startTimer() {
-    tens++
+    tens++;
 
     if (tens > 99) {
-        // seonds 1 올리기
-        seconds++
-        // appendSeconds 에도 반영하기
-        appendSeconds.innerText = seconds
-        // tens, appendTens => 0
-        tens = 0
-        appendTens.innerText = tens
+        seconds++;
+        appendSeconds.innerText = seconds;
+        tens = 0;
+        appendTens.innerText = tens;
     } else {
-        appendTens.innerText = tens
+        appendTens.innerText = tens;
     }
 }
