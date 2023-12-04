2ffa87d236374d270ff71dbf1724bebbed34f357
heekwang <ekzm7040@naver.com>
2023-12-04 17:47:24 +0900

    type &interface

---
+++ typescript-code/04.type,interface/src/main.ts
@@ -0,0 +1,43 @@
+// interface Animal {
+//     name: string
+// }
+
+// interface Bear extends Animal {
+//     honey: boolean
+// }
+
+// const bear1: Bear = {
+//     name: 'honey bear',
+//     honey: true
+// }
+
+// type Animal = {
+//     name: string
+// }
+
+// type Bear = Animal & {
+//     honey: boolean
+// }
+
+// const bear1: Bear = {
+//     name: "honey bear",
+//     honey: true
+// }
+
+// interface Animal {
+//     name: string;
+// }
+
+// interface Animal {
+//     honey: boolean
+// }
+
+// const bear1: Animal = {
+//     name: 'hney bear',
+//     honey: true
+// }
+
+type Animal = {
+    name: string;
+}
+
