152238fb352e33073c514129fd12efb75fc6dd29
heekwang <ekzm7040@naver.com>
2024-01-04 16:10:55 +0900

    styled Component로 footer만들기

--- react-netflix-clone/src/App.js
+++ react-netflix-clone/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Banner from "./components/Banner"
 import Row from "./components/Row";
 import requests from "./api/requests";
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -36,6 +37,7 @@ function App() {
         id="CM"
         fetchUrl={requests.fetchComedyMovies}
       />
+      <Footer />
     </div>
   );
 }
