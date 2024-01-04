1d6bebdf71791efd712f23d7dba4725e70149389
heekwang <ekzm7040@naver.com>
2024-01-04 15:21:49 +0900

    영상 나열을 위한 row 컴포넌트 생성하기

--- react-netflix-clone/src/App.js
+++ react-netflix-clone/src/App.js
@@ -1,12 +1,41 @@
 import Nav from "./components/Nav"
 import './App.css';
 import Banner from "./components/Banner"
+import Row from "./components/Row";
+import requests from "./api/requests";
 
 function App() {
   return (
     <div className="app">
       <Nav />
       <Banner />
+
+      <Row
+        title="NETFLIX ORIGINALSERVER"
+        id="NO"
+        fetchUrl={requests.fetchNetflixOriginals}
+        isLargeRow
+      />
+      <Row
+        title="TrendingNow"
+        id="TN"
+        fetchUrl={requests.fetchTrending}
+      />
+      <Row
+        title="Top Rate"
+        id="TR"
+        fetchUrl={requests.fetchTopRated}
+      />
+      <Row
+        title="Action Movies"
+        id="AM"
+        fetchUrl={requests.fetchActionMovies}
+      />
+      <Row
+        title="Comedy Movies"
+        id="CM"
+        fetchUrl={requests.fetchComedyMovies}
+      />
     </div>
   );
 }
