1e7b1f0027e54a6954b17b3e62f4991f0258b42a
heekwang <ekzm7040@naver.com>
2024-01-04 15:32:49 +0900

    슬라이드 기능 추가하기

--- react-netflix-clone/src/components/Row.js
+++ react-netflix-clone/src/components/Row.js
@@ -21,7 +21,9 @@ export default function Row({ isLargeRow, title, id, fetchUrl }) {
             <h2>{title}</h2>
             <div className='slider'>
                 <div className='slider__arrow-left'>
-                    <span className='arrow'>{"<"}</span>
+                    <span className='arrow' onClick={() => {
+                        document.getElementById(id).scrollLeft -= window.innerWidth - 80;
+                    }}>{"<"}</span>
                 </div>
                 <div id={id} className='row__posters'>
                     {movies.map(movie => (
@@ -33,7 +35,11 @@ export default function Row({ isLargeRow, title, id, fetchUrl }) {
                         />
                     ))}\
                 </div>
-                <div className='slider__arrow-right'>{">"}</div>
+                <div className='slider__arrow-right'
+                    onClick={() => {
+                        document.getElementById(id).scrollLeft += window.innerWidth + 80;
+                    }}
+                >{">"}</div>
 
 
             </div>
