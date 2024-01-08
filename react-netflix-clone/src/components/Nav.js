f3f32650c4cc2f12abc4b70acb59d97035ae14be
heekwang <ekzm7040@naver.com>
2024-01-08 14:10:09 +0900

    useLocation을 이용한 검색 페이지 구현

--- react-netflix-clone/src/components/Nav.js
+++ react-netflix-clone/src/components/Nav.js
@@ -1,10 +1,18 @@
 import React, { useState, useEffect } from 'react'
+import { useNavigate } from "react-router-dom"
 import netflixLogo from '../imges/netflixLogo.jpg'
 import userLogo from '../imges/userLogo.png'
 import "./Nav.css"
 
 export default function Nav() {
-    const [show, setShow] = useState(false)
+    const [show, setShow] = useState(false);
+    const [searchValue, setSearchValue] = useState("");
+    const navigate = useNavigate();
+
+    const handleChange = (e) => {
+        setSearchValue(e.target.value)
+        navigate(`/search?q=${e.target.value}`)
+    }
 
     useEffect(() => {
         window.addEventListener("scroll", () => {
@@ -24,6 +32,7 @@ export default function Nav() {
     return (
         <nav className={`nav ${show && "nav__black"}`}>
             <img alt='Netflix logo' src={netflixLogo} className="nav__logo" onClick={() => window.location.reload()} />
+            <input value={searchValue} onChange={handleChange} className='nav__input' type='text' placeholder='영화를 검색하주세요' />
             <img alt='user logo' src={userLogo} className="nav__avatar" onClick={() => window.location.reload()} />
 
         </nav>
--- react-netflix-clone/src/pages/SearchPage/index.js
+++ react-netflix-clone/src/pages/SearchPage/index.js
@@ -1,7 +1,40 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { useLocation } from 'react-router-dom'
+import axios from '../../api/axios'
 
 export default function SearchPage() {
+    const [searchResult, setSearchResult] = useState([])
+
+
+    console.log("useLocation: ", useLocation())
+    const useQuery = () => {
+        return new URLSearchParams(useLocation().search)
+    }
+
+    let query = useQuery()
+    const searchTerm = query.get("q")
+    console.log("searchTerm", searchTerm)
+
+    useEffect(() => {
+        if (searchTerm) {
+            fetchSearchMovie(searchTerm)
+        }
+    }, [searchResult])
+
+    const fetchSearchMovie = async (searchTerm) => {
+        try {
+            const request = await axios.get(
+                `/search/multi?include_adult=false&query=${searchTerm}`
+            )
+            console.log("request", request)
+            setSearchResult(request.data.results)
+        } catch (error) {
+            console.log("error", error)
+        }
+    }
+
     return (
-        <div>SearchPage</div>
+        <div></div>
+
     )
 }
