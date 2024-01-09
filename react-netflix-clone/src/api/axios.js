e8ac650ea3a461167b51aeb3f788a98a9323f6bc
heekwang <ekzm7040@naver.com>
2024-01-09 15:41:54 +0900

    github 배포하기

--- react-netflix-clone/src/api/axios.js
+++ react-netflix-clone/src/api/axios.js
@@ -3,7 +3,7 @@ import axios from "axios"
 const instance = axios.create({
     baseURL: 'http://api.themoviedb.org/3',
     params: {
-        api_key: '3def14819e849b1546485a7c79b9931e',
+        api_key: process.env.REACT_APP_MOVIE_DB_API_KEY,
         language: 'ko-KR',
     }
 })
